51a18d5d43c56a3043a6e3f60291984360bfb27a
Cooluke75 <luke.lee7575@gmail.com>
2018-01-18 16:21:30 -0800

    Updated actor.php for quote display and updated changelog
    Signed-off-by: Cooluke75 <luke.lee7575@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,10 @@
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
+                <div>
+                	<h3>Actor Name: {who}</h3>
+                	<h4>Role: {role}</h4>
+                	<h4>Quote: {what}</h4>
+                </div>
         </div>
 </div>
